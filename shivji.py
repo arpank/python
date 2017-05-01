eb4e143631818d51771b83b8c02f26b9e773ba72
arpank <arpank200@gmail.com>
2017-04-30 20:55:59 -0400

    test

---
+++ shivji.py
@@ -0,0 +1,26 @@
+# This program prints Hello, world!
+
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+num23 = input('Enter third number: ')
+
+# Add two numbers
+sum = float(num1) + float(num2) + float(num23)
+
+# Display t
+
+print('The sum of {0} and {1} and {2} is {3}'.format(num1, num2,num23, sum))
+
+var = 100
+
+if ( var  == 100 ) : print ("Value of expression is 100")
+
+tup = ('physics', 'chemistry', 1997, 2000);
+
+print (tup)
+del tup;
+
+print (tup)
+
+
